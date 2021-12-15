1144d60746762d428fff512c77cae6f49e3552d1
Jackson Merma <mermaportocarreroj@gmail.com>
2021-12-14 21:10:45 -0500

    prueba

---
+++ cses/hola.cpp
@@ -0,0 +1,23 @@
+/** 
+* @author      : Jackson Fernando Merma Portocarrero (jmermap@unsa.edu.pe)
+* @created     : 14/12/2021
+* @filename    : hola
+* @description : _
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ld long double
+#define endl "\n"
+#define PI 3.1415926535897932384626
+
+int main(){
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	
+	cout<<"hola"<<endl;
+
+	return 0;
+}
+
